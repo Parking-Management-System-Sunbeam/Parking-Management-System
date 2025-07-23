55b1b9b0dcaf7ee55d20e4157fb3d1fc75068587
Chaitanya Patil <chaitanyapatil236@gmail.com>
2025-07-23 17:46:01 +0530

    Search Component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Login from './Pages/Auth/Login'
-
+import SearchComponent from './Components/SearchComponent'
 const App = () => {
   return (
     <div className=''>
-    <Login />
+    {/* <Login /> */}
+    <SearchComponent/>
     </div>
   )
 }
