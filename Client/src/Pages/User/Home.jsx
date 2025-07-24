896bdd90bd28144b2ca99d2ebd8df9064d9f8b5a
RushikeshGangapurkar15 <rushikeshgangapurkar7772@gmail.com>
2025-07-24 16:31:01 +0530

    UI fix search component

--- Client/src/Pages/User/Home.jsx
+++ Client/src/Pages/User/Home.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
+import SearchComponent from "../../Components/SearchComponent";
 
 function Home() {
   return (
     <div className="flex">
+      {/* sidebar */}
       <div className=" flex-1/6">
 
       <Sidebar />
       </div>
-      <div className="flex-4/5 p-4">
-        <h1 className="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
+
+      {/* Main page */}
+      <div className="flex-4/5 p-4 items-center mt-9">
+      <SearchComponent />
+        </div>
+
+        <div className="flex-1/6 p-9">
+          <div className="w-20 h-20 bg-gray-800 rounded-full"></div>
         </div>
     </div>
   );
