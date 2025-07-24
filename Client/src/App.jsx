21fa02aaf99168e2740c83d486ae16e0085f9746
Kunal Idhate <kunalidhate9534@gmail.com>
2025-07-24 21:16:55 +0530

    Details page added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,18 +7,18 @@ import { Route, Routes } from "react-router-dom";
 import AboutUs from "./Pages/User/AboutUs";
 import Bookings from "./Pages/User/Bookings";
 import Details from "./Pages/User/Details";
-import React from 'react'
-import Login from './Pages/Auth/Login'
-import Home from './Pages/User/Home'
-import SearchComponent from './Components/SearchComponent'
-import SignUp from './Pages/Auth/SignUp'
-import { Route, Routes } from 'react-router-dom';
-import AboutUs from './Pages/User/AboutUs'
-import Bookings from './Pages/User/Bookings'
-import Payment from './Pages/User/Payment'
-import OrderSummary from './Components/OrderSummary'
-import PaymentComponent from './Components/PaymentComponent'
-import PayDetails from './Components/PayDetails'
+import React from "react";
+import Login from "./Pages/Auth/Login";
+import Home from "./Pages/User/Home";
+import SearchComponent from "./Components/SearchComponent";
+import SignUp from "./Pages/Auth/SignUp";
+import { Route, Routes } from "react-router-dom";
+import AboutUs from "./Pages/User/AboutUs";
+import Bookings from "./Pages/User/Bookings";
+import Payment from "./Pages/User/Payment";
+import OrderSummary from "./Components/OrderSummary";
+import PaymentComponent from "./Components/PaymentComponent";
+import PayDetails from "./Components/PayDetails";
 
 const App = () => {
   return (
